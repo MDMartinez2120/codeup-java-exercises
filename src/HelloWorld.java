2fbfbd868daea68c86d45defa97cdd58581d145a
Matthew Martinez <matt.martinez2120@gmail.com>
2022-03-24 16:30:15 -0500

    finished the syntax java exercise.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,48 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        int myFavoriteNumber = 22;
+        System.out.println(myFavoriteNumber);
+
+        String myString ="3.14159";
+        System.out.println(myString);
+
+        float myNumber = 3.14F;
+        System.out.println(myNumber);
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+
+//        int three = (int) "three";
+//        System.out.println("three = " + three);
+
+//           int x = 4;
+//           x += 5;
+//        System.out.println("x = " + x);
+
+//            int x = 3;
+//            int y = 4;
+
+//            y *= x;
+//        System.out.println("y = " + y);
+        
+        int x = 10;
+        int y = 2;
+
+//        x /= y;
+//        System.out.println("x = " + x);
+
+        x = x / y;
+        y = y - x;
+        System.out.println("y = " + y);
+
     }
 }
