3279b385b11d35d4330f46c08ca34c636d05b2f6
Matthew Martinez <matt.martinez2120@gmail.com>
2022-03-29 15:17:29 -0500

    Finished part 1 of the strings exercise.

---
+++ src/StringExercises.java
@@ -0,0 +1,18 @@
+public class StringExercises {
+    public static void main(String[] args) {
+//        String Message = "We don't need no education\n" +
+//                "We don't need no thought control";
+//                System.out.println(Message);
+
+//        String CheckIt = "Check \"this\" out!, \"s inside of \"s!";
+//        System.out.println(CheckIt);
+
+//        String CDrive = "In windows, the main drive is usually C:\\";
+//        System.out.println(CDrive);
+
+        String SlashItUp = "I can do backslashes \\, double backslashes \\\\,\n" +
+                "and the amazing triple backslash \\\\\\!";
+        System.out.println(SlashItUp);
+
+    }
+}
