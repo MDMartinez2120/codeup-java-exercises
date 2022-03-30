f4a1ff234b969e0d3d1d484ebac8622d59024ce8
Matthew Martinez <matt.martinez2120@gmail.com>
2022-03-30 12:10:46 -0500

    finished exercise 1 for the java methods exercise.

---
+++ src/MethodsExercises.java
@@ -0,0 +1,34 @@
+public class MethodsExercises {
+    public static void main(String[] args) {
+        addNums(3, 3);
+        subNums(10, 5);
+        mulNums(8,4);
+        divNums(20, 5);
+    }
+    public static void addNums(int num1, int num2){
+        int i = num1 + num2;
+        System.out.println(i);
+    }
+
+    public static void subNums(int num1, int num2){
+        int i = num1 - num2;
+        System.out.println(i);
+    }
+
+    public static void mulNums(int num1, int num2){
+        int i = num1 * num2;
+        System.out.println(i);
+    }
+
+    public static void divNums(int num1, int num2){
+        int i = num1 / num2;
+        System.out.println(i);
+    }
+
+};
+
+
+
+
+
+
