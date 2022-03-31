4ef23b1c33bd007481979961ce271c966d040f25
Matthew Martinez <matt.martinez2120@gmail.com>
2022-03-31 09:25:49 -0500

    finished the java warmup.

---
+++ src/JavaWarmups.java
@@ -0,0 +1,24 @@
+public class JavaWarmups {
+    public static void main(String[] args) {
+
+    }
+    public static char returnFirstCapitalLetter(String s){
+        for (int i = 0; i < s.length(); i += 1){
+            if (s.charAt(i) == Character.toUpperCase(s.charAt(i))){
+                return s.charAt(i);
+            }
+        }
+    }
+    return '';
+}
+
+//    Create a method, returnFirstCapitalLetter, that takes in a string and returns the first capital letter as a char. If no capital letter is found, return a single space character ' '. Assume only letters in the input string and all possible inputs will have at least one letter.
+//
+//        Examples...
+//
+//        returnFirstCapitalLetter("hellO") // returns 'O'
+//        returnFirstCapitalLetter("hello") // returns ' '
+//        returnFirstCapitalLetter("hEllo") // returns 'E'
+//        returnFirstCapitalLetter("hELlO") // returns 'E'
+//        returnFirstCapitalLetter("H") // returns 'H'
+
