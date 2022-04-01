6ff7fd9f92c4bd7cf7ceee206e073bd28aa8fa16
Matthew Martinez <matt.martinez2120@gmail.com>
2022-04-01 08:09:28 -0500

    finished part of A of the mini assignment.

--- src/Dish.java
+++ src/Dish.java
@@ -1,31 +1,59 @@
-public class Dish {
-    public static void main(String[] args) {
-        Post dish1 = new Post();
-        dish1.nameOfDish = "Brisket";
-        dish1.costInCents = 40_000;
-        dish1.wouldRecommend = false;
-        System.out.println(dish1.printSummary());
+class Dish {
+    private int costInCents;
+    private String nameOfDish;
+    private boolean wouldRecommend;
 
+
+    public void printSummary(){
+        System.out.printf("Cost:  %d \nName: %s\nRecommended: %b", costInCents, nameOfDish, wouldRecommend);
+    }
+    public String getNameOfDish(String nameOfDish) {
+        return nameOfDish;
     }
 
-}
-
-class Post {
-    public int costInCents;
-    public String nameOfDish;
-    public boolean wouldRecommend;
-
-    public String  printSummary() {
-        return  "\nCost in cents: " + costInCents +  "\n"+
-                "Name of dish: " + nameOfDish + "\n" +
-                "Would Recommend: " + wouldRecommend + "\n";
+    public void setNameOfDish(String nameOfDish){
+        this.nameOfDish = nameOfDish;
     }
 
-    public String updatePrintSummary(int costInCents, String nameOfDish, boolean wouldRecommend){
+    public void getCostInCents(int costInCents){
+        this.costInCents = costInCents;
+    }
+
+    public void setCostInCents(int costInCents){
+        this.costInCents = costInCents;
+    }
+
+    public void getWouldRecommend(boolean wouldRecommend){
+        this.wouldRecommend = wouldRecommend;
+    }
+
+    public void setWouldRecommend(boolean wouldRecommend){
+        this.wouldRecommend = wouldRecommend;
+    }
+
+    public void dish(int costInCents, String nameOfDish, boolean wouldRecommend){
         this.costInCents = costInCents;
         this.nameOfDish = nameOfDish;
         this.wouldRecommend = wouldRecommend;
-
-        return this.printSummary();
     }
-};
+
+    public static class DishTest{
+        public static void main(String[] args) {
+            Dish dish1= new Dish(); //This creates a new object
+            dish1.nameOfDish= "Brisket";
+            dish1.costInCents = 200;
+            dish1.wouldRecommend= false;
+
+            dish1.printSummary();
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
