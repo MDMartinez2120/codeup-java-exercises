d377b795fff5fdde9600be7c1f4efa5385b91b90
Matthew Martinez <matt.martinez2120@gmail.com>
2022-04-04 13:54:20 -0500

    added adjectives and nouns to my server name generator.

---
+++ src/ServerNameGenerator.java
@@ -0,0 +1,10 @@
+public class ServerNameGenerator {
+    public static void main(String[] args) {
+        //Create two arrays whose elements are strings: one with at least 10 adjectives, another with at least 10 nouns.
+
+        String[] adjectives = {"cold", "hot", "cool", "warm", "tall", "short", "big", "small", "wet", "dry"};
+
+        String[] nouns = {"car", "wheel", "paint", "hand", "stick", "shoe", "bike", "bench", "fountain", "shrub"};
+
+    }
+}
