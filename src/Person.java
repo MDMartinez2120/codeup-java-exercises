e4c183cd95dad8e167c47398ed354e75e7075b4a
Matthew Martinez <matt.martinez2120@gmail.com>
2022-04-04 21:09:39 -0500

    finished the first part of oop exercise.

--- src/Person.java
+++ src/Person.java
@@ -1,21 +1,47 @@
 public class Person {
-    public String name;
+    public static void main(String[] args) {
+//        Person person1 = new Person("John");
+//        Person person2 = new Person("John");
+//        System.out.println(person1.getName().equals(person2.getName()));
+//        System.out.println(person1 == person2);
+
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1.getName());
+//        System.out.println(person2.getName());
+//        person2.setName("Jane");
+//        System.out.println(person1.getName());
+//        System.out.println(person2.getName());
+
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1.getName());
+//        System.out.println(person2.getName());
+//        person2.setName("Jane");
+//        System.out.println(person1.getName());
+//        System.out.println(person2.getName());
+    }
+    private String name;
 
     public Person(String name) {
         this.name = name;
     }
 
+    //TODO: return the person's name
     public String getName(){
-//TODO: return the person's name
         return name;
     }
 
+    //TODO: change the name field to the passed value
     public void setName(String name){
-//TODO: change the name field to the passed value
         this.name = name;
     }
+
+    //TODO: print a message to the console using the person's name
     public void sayHello(){
-//TODO: print a message to the console using the person's name
         String.format("%s says hello!", this.name);
     }
 }
+
+
+
