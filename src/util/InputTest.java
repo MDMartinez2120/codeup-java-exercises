904ca6a9baec3c8d386ba15384a51fe33bd434ad
Matthew Martinez <matt.martinez2120@gmail.com>
2022-04-07 16:53:01 -0500

    finished the exceptions exercises.

--- src/util/InputTest.java
+++ src/util/InputTest.java
@@ -10,5 +10,12 @@ public class InputTest {
         }catch (NumberFormatException nfe){
             System.out.println("no way jose!" + nfe);
         }
+
+        try{
+            Input getDouble = new Input();
+            System.out.println(getDouble.getDouble(1,20));
+        }catch (NumberFormatException nfe){
+            System.out.println("no way jose!" + nfe);
+        }
     }
 }
