648040b8b7641619098b13e41ce9a933a33cfa24
Matthew Martinez <matt.martinez2120@gmail.com>
2022-04-07 09:33:07 -0500

    finished a question for the java practice assessment questions.

---
+++ src/Practice.java
@@ -0,0 +1,17 @@
+public class Practice {
+    public static int subtractTen(int x, int y){
+        return (x - y) - 10;
+    }
+
+    public static double average(){
+        return 0;
+    }
+
+    public static class pet {
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(subtractTen(100, 50));
+    }
+}
