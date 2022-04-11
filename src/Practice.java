a004a7ae59b4b90ad544af275de92221d3d2157b
Matthew Martinez <matt.martinez2120@gmail.com>
2022-04-11 15:00:06 -0500

    finished a few answers on the practice assessment questions.

--- src/Practice.java
+++ src/Practice.java
@@ -7,16 +7,13 @@ public class Practice {
         return (x - y) - 10;
     }
 // 3. Create a public static method *average* in the Practice class that takes an array of integers and returns their average as a double.
-//    public static double average(){
-//        double[] myArray = new double[3];
-//        myArray[0] = 6.9;
-//        myArray[1] = 2.5;
-//        myArray[2] = 11.5;
-//        return 0;
-//        for (int i = 0; i < myArray.length; i++){
-//
-//        }
-//    }
+    public static double average(double[] nums){
+        double total = 0;
+        for(double i = 0; i < nums.length; i++){
+            total += nums[(int) i];
+        }
+        return total / nums.length;
+    }
 // 4. Create a class called Pet with two String instance variables, name and type.
 
      public class pet {
@@ -43,19 +40,22 @@ public class Practice {
 }
 
 // 6. Create a PetDog class that inherits from Pet.
-    public class PetDog extends pet{
-// 7. In PetDog, create a boolean instance variable, trained.
-       public boolean trained;
+    public class PetDog extends pet {
+    // 7. In PetDog, create a boolean instance variable, trained.
+    public boolean trained;
 
-//8. Write a constructor for PetDog that sets its name, type, and trained properties.
+    //8. Write a constructor for PetDog that sets its name, type, and trained properties.
     public PetDog(boolean trained, String name, String type) {
         this.trained = trained;
         this.name = name;
         this.type = type;
 // 9. Write an instance method in PetDog  called isTrained that returns the boolean value of the trained property.
-        public boolean isTrained = new
+
     }
-}
+
+    public boolean isTrained() {
+        return trained;
+    }
 
 // 10. Create an interface named Companion. The Companion interface should specify an instance method named snuggle that has no arguments and returns a String. Change your PetDog class so it implements the Companion interface. The implementation of the snuggle method on the PetDog class should return "name wants to snuggle", where name is the inherited instance property.
 
@@ -63,21 +63,21 @@ public class Practice {
 // 11. Create a PetDogTest class, instantiate a PetDog, and verify that the snuggle method works correctly.
 
 
-
 // 12. In your Practice class, create a static method named multiplyAll. It should accept an integer and an ArrayList of integers. It should take the ArrayList argument and multiply all its elements by the value of the integer argument.
-
-
 
 
 // 12b. In your Pet class, create a static method named allSnuggle. It should accept an ArrayList of PetDog objects and output the result of calling the snuggle method on all of them. Create an ArrayList with at least three PetDog instances in the PetDogTest class. Call the allSnuggle method and pass it the ArrayList. Verify that the allSnuggle method works correctly.
 
 
-
-
     ///////////////////////////////////////Main////////////////////////////////
     public static void main(String[] args) {
         System.out.println(subtractTen(100, 50));
+        System.out.println();
+        double[] myArray = new double[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        System.out.println(average(myArray));
+        System.out.println();
     }
+   }
 }
 
 
